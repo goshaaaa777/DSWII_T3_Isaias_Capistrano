6fbc982bb5b4560763a73d4cd64a5c383dae279c
goshaaaa777 <isaiasddx322z@gmail.com>
2024-07-02 18:06:39 -0500

    inicio

---
+++ src/main/java/cibertec/edu/pe/DSWII_T3_Isaias_Capistrano/DswiiT3IsaiasCapistranoApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_Isaias_Capistrano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3IsaiasCapistranoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3IsaiasCapistranoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DSWII_T3_Isaias_Capistrano/DswiiT3IsaiasCapistranoApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_Isaias_Capistrano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3IsaiasCapistranoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
